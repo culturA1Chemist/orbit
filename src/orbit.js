49bbaa8b3e85de6cc911d7bd0f354d7563ac4b3c
Juan Martin <martinmuda@gmail.com>
2024-12-24 15:57:05 +0300

    complete o-text o-arc fussion

--- src/orbit.js
+++ src/orbit.js
@@ -1,10 +1,8 @@
 import { OrbitProgress } from './js/orbit-progress.js'
 import { OrbitArc } from './js/orbit-arc.js'
-//import { OrbitText } from './js/orbit-text.js'
 import { Orbit } from './js/orbit-resize.js'
 
 customElements.define('o-progress', OrbitProgress)
 customElements.define('o-arc', OrbitArc)
-//customElements.define('o-text', OrbitText)
 
 window.Orbit = Orbit
